ab3e5101a61b245799ad27a1a2b9db0002e0f704
Rahim Rajwani <69324892+rahim-rajwani@users.noreply.github.com>
2020-12-23 09:39:56 +1100

    Update ViewController.swift

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var diceImageView2: UIImageView!
     
     var leftDiceNumber = 1
+    var rightDiceNumber = 5
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,7 +32,16 @@ class ViewController: UIViewController {
     }
 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
+        //leftDiceNumber = 1
         diceImageView1.image = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")][leftDiceNumber]
+        print("leftDiceNumber at beginning = \(leftDiceNumber)")
+        diceImageView2.image = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")][rightDiceNumber]
+        print("rightDiceNumber at the beginning = \(rightDiceNumber)")
+        leftDiceNumber = leftDiceNumber + 1
+        print("leftDiceNumber at the end = \(leftDiceNumber)")
+        rightDiceNumber = rightDiceNumber - 1
+        print ("rightDiceNumber at the end = \(rightDiceNumber)")
+        
         
         
         //diceImageView1.image = #imageLiteral(resourceName: "DiceFour")
