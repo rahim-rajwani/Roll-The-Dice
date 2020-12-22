e7949c4fa83e61fbf26eda55dc913c568dcf801e
Rahim Rajwani <69324892+rahim-rajwani@users.noreply.github.com>
2020-12-22 19:38:29 +1100

    diceImageViews Added

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,9 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        // Remember the formula - Who.What= Value
+        // WHO - needs to be changed
+        // WHAT - property needs to be changed
+        // VALUE - what it should be changed to
+        
+        diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
     }
 
 
