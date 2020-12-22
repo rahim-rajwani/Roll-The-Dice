842dafabfe9779e84f7e416aad65000f1c54f20e
Rahim Rajwani <69324892+rahim-rajwani@users.noreply.github.com>
2020-12-23 08:57:25 +1100

    Update ViewController.swift

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
     
+    var leftDiceNumber = 1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -24,13 +26,16 @@ class ViewController: UIViewController {
         
         //diceImageView1.alpha = 0.5 (change transperancy)
         
-        diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
-        diceImageView2.image = #imageLiteral(resourceName: "DiceTwo")
+        //diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
+        //diceImageView2.image = #imageLiteral(resourceName: "DiceTwo")
     }
 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
-        diceImageView1.image = #imageLiteral(resourceName: "DiceFour")
-        diceImageView2.image = #imageLiteral(resourceName: "DiceFour")
+        diceImageView1.image = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")][leftDiceNumber]
+        
+        
+        //diceImageView1.image = #imageLiteral(resourceName: "DiceFour")
+        //diceImageView2.image = #imageLiteral(resourceName: "DiceFour")
     }
     
 }
