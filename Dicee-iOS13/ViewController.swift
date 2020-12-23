8aafe772a73ea43323f8af3033e0c96f0ab61371
Rahim Rajwani <69324892+rahim-rajwani@users.noreply.github.com>
2020-12-24 10:56:42 +1100

    Update ViewController.swift

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -18,12 +18,17 @@ class ViewController: UIViewController {
     
     @IBAction func rollButtonPressed(_ sender: UIButton) {
         
-        var diceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+        let diceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+        //changed diceArray from 'var' to 'let' given it no longer requires changes.
+        //remember, if you want to vary it again, you'll need to make it a var (from let).
         
         //leftDiceNumber = 1
         diceImageView1.image = diceArray[Int.random (in: 0...5)]
+        //another way of writing this line (more succinctly) is: diceImageView1.image = diceArray.randomElement()
+        //this way is in fact a lot better than the way you've employed - but both will produce the same result
         print("leftDiceNumber at beginning = \(leftDiceNumber)")
         diceImageView2.image = diceArray[Int .random(in: 0...5)]
+        //rahim - again, same thing here - you can change it to .randomElement() :) 
         print("rightDiceNumber at the beginning = \(rightDiceNumber)")
 //        leftDiceNumber += 1
 //        print("leftDiceNumber at the end = \(leftDiceNumber)")
