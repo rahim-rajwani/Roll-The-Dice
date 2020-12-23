2e79a70c6558db0e8f5612cdbea7ce735a809d45
Rahim Rajwani <69324892+rahim-rajwani@users.noreply.github.com>
2020-12-24 10:39:03 +1100

    Update ViewController.swift

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -16,32 +16,18 @@ class ViewController: UIViewController {
     var leftDiceNumber = 1
     var rightDiceNumber = 5
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        // Do any additional setup after loading the view.
-        // Remember the formula - Who.What= Value
-        // WHO - needs to be changed
-        // WHAT - property needs to be changed
-        // VALUE - what it should be changed to
-        
-        //diceImageView1.alpha = 0.5 (change transperancy)
-        
-        //diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
-        //diceImageView2.image = #imageLiteral(resourceName: "DiceTwo")
-    }
-
     @IBAction func rollButtonPressed(_ sender: UIButton) {
         //leftDiceNumber = 1
-        diceImageView1.image = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")][leftDiceNumber]
+        diceImageView1.image = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")][Int.random (in: 0...5)]
         print("leftDiceNumber at beginning = \(leftDiceNumber)")
-        diceImageView2.image = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")][rightDiceNumber]
+        diceImageView2.image = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")] [Int .random(in: 0...5)]
         print("rightDiceNumber at the beginning = \(rightDiceNumber)")
-        leftDiceNumber = leftDiceNumber + 1
+        leftDiceNumber += 1
         print("leftDiceNumber at the end = \(leftDiceNumber)")
-        rightDiceNumber = rightDiceNumber - 1
+        rightDiceNumber -= 1
         print ("rightDiceNumber at the end = \(rightDiceNumber)")
         
+        print (Int.random(in: 1...10))
         
         
         //diceImageView1.image = #imageLiteral(resourceName: "DiceFour")
